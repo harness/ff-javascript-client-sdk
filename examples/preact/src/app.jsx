1c7d45a6e549b850162630ecfe67b34d35812891
Tan Nhu <tan@harness.io>
2021-05-20 14:52:08 -0700

    Add Preact example

---
+++ examples/preact/src/app.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'preact/hooks'
+import { initialize, Event } from '@harnessio/ff-javascript-client-sdk'
+
+export const App = () => {
+  const [featureFlags, setFeatureFlags] = useState({})
+
+  useEffect(() => {
+    const cf = initialize(
+      '50e52f07-ec4b-4b4a-8743-fd5050c31e1c',
+      {
+        identifier: 'Harness',
+        attributes: {
+          lastUpdated: Date(),
+          host: location.href
+        }
+      },
+      {
+        debug: true,
+        baseUrl: 'http://35.199.167.179/api/1.0', // QA
+        eventUrl: 'http://34.83.236.94/api/1.0'
+      }
+    )
+
+    cf.on(Event.READY, flags => {
+      setFeatureFlags(flags)
+    })
+
+    cf.on(Event.CHANGED, flagInfo => {
+      if (flagInfo.deleted) {
+        setFeatureFlags(currentFeatureFlags => {
+          delete currentFeatureFlags[flagInfo.flag]
+          return { ...currentFeatureFlags }
+        })
+      } else {
+        setFeatureFlags(currentFeatureFlags => ({ ...currentFeatureFlags, [flagInfo.flag]: flagInfo.value }))
+      }
+    })
+
+    return () => {
+      cf.close()
+    }
+  }, [])
+
+  return <pre>{JSON.stringify(featureFlags, null, 2)}</pre>
+}
