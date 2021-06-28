2192fb51aa0e0aa684add50f67a4ff2d7690bc3d
Tan Nhu <tan@harness.io>
2021-06-28 14:27:10 -0700

    Suppress error logging when stream is trying to reconnect

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ import type {
 import { Event } from './types'
 import { logError, defaultOptions, METRICS_FLUSH_INTERVAL } from './utils'
 
-const SDK_VERSION = '1.4.1'
+const SDK_VERSION = '1.4.2'
 const METRICS_VALID_COUNT_INTERVAL = 500
 const fetch = globalThis.fetch
 const EventSource = EventSourcePolyfill
