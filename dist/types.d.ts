c2b43b69bc0c9f8aab49b4e4f95064346b0d8658
Kevin Nagurski <Kevin.nagurski@harness.io>
2023-01-24 16:18:06 +0000

    feat: [FFM-6489]: Allow side-loading of evaluations (#64)

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -15,7 +15,12 @@ export declare enum Event {
     CONNECTED = "connected",
     DISCONNECTED = "disconnected",
     CHANGED = "changed",
-    ERROR = "error"
+    ERROR = "error",
+    ERROR_METRICS = "metrics error",
+    ERROR_AUTH = "auth error",
+    ERROR_FETCH_FLAGS = "fetch flags error",
+    ERROR_FETCH_FLAG = "fetch flag error",
+    ERROR_STREAM = "stream error"
 }
 export declare type VariationValue = boolean | string | number | object | undefined;
 export interface Evaluation {
@@ -31,6 +36,11 @@ export interface EventCallbackMapping {
     [Event.DISCONNECTED]: () => void;
     [Event.CHANGED]: (flag: Evaluation) => void;
     [Event.ERROR]: (error: unknown) => void;
+    [Event.ERROR_AUTH]: (error: unknown) => void;
+    [Event.ERROR_FETCH_FLAGS]: (error: unknown) => void;
+    [Event.ERROR_FETCH_FLAG]: (error: unknown) => void;
+    [Event.ERROR_STREAM]: (error: unknown) => void;
+    [Event.ERROR_METRICS]: (error: unknown) => void;
 }
 export declare type EventOnBinding = <K extends keyof EventCallbackMapping>(event: K, callback: EventCallbackMapping[K]) => void;
 export declare type EventOffBinding = <K extends keyof EventCallbackMapping>(event?: K, callback?: EventCallbackMapping[K]) => void;
@@ -39,6 +49,7 @@ export interface Result {
     off: EventOffBinding;
     variation: (identifier: string, defaultValue: any) => VariationValue;
     close: () => void;
+    setEvaluations: (evaluations: Evaluation[]) => void;
 }
 export interface Options {
     baseUrl?: string;
