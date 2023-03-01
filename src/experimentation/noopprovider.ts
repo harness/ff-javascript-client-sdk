8984b9d4e7c20002205a94115d399766e80822ad
Gareth Fleming <gareth@joocy.com>
2023-03-01 10:25:27 +0000

    Updated NoOpProvider to use the provider log function.

--- src/experimentation/noopprovider.ts
+++ src/experimentation/noopprovider.ts
@@ -11,15 +11,11 @@ export default class NoOpExperimentProvider extends BaseProvider {
 
   initialize(config: ExperimentProviderConfig) {
     super.initialize(config);
-    if (this.debug) {
-      console.log(`*** Initializing the NOOP provider with ${config.apiKey} ***`);
-    }
+    this.log(`*** Initializing the NOOP provider with ${config.apiKey} ***`);
   }
 
   startExperiment(flagIdentifier: string, variation: VariationValue, _target?: Target) {
-    if (this.debug) {
-      console.log(`*** NOOP Experiment for ${flagIdentifier} with variation ${variation} ***`);
-    }
+    this.log(`*** NOOP Experiment for ${flagIdentifier} with variation ${variation} ***`);
   }
 
 }
