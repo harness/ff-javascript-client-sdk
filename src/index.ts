30993edd698914a03c99e5023008b8105cc78fef
Andrew Bell <115623869+andybharness@users.noreply.github.com>
2023-08-17 12:36:57 +0100

    [FFM-9054] - An implementation of SSE using fetch()
    What
    Remove XMLHttpRequest() and use fetch() instead.
    
    This is an experimential PR, some features are absent:
    - SSE timeout handling
    - HTTP error code handling
    - Restarting SSE
    
    Why
    This is a discovery ticket to look at implementing fetch() instead of XMLHttpRequest() and to see how feasible doing that is. Fetch has a cleaner and modern API which can help with code readability.
    
    Testing
    Manual

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ import { defaultOptions, defer, logError, MIN_EVENTS_SYNC_INTERVAL } from './uti
 import { loadFromCache, removeCachedEvaluation, saveToCache, updateCachedEvaluation } from './cache'
 import { addMiddlewareToFetch } from './request'
 
+
 const SDK_VERSION = '1.15.0'
 const SDK_INFO = `Javascript ${SDK_VERSION} Client`
 const METRICS_VALID_COUNT_INTERVAL = 500
@@ -504,88 +505,51 @@ const initialize = (apiKey: string, target: Target, options?: Options): Result =
       }
     }
 
-    const getRandom = (min, max) => {
-      return Math.round(Math.random() * (max - min) + min)
-    }
-
-    const onConnected = () => {
-      logDebug('Stream connected')
-      eventBus.emit(Event.CONNECTED)
-    };
-
-    const onDisconnect = () => {
-      clearInterval(readTimeoutCheckerId)
-      const reconnectDelayMs = getRandom(1000, 10000)
-      logDebug('Stream disconnected, will reconnect in ' + reconnectDelayMs + 'ms')
-      eventBus.emit(Event.DISCONNECTED)
-      setTimeout(startStream, reconnectDelayMs)
-    };
-
-    const onFailed = (msg: string) => {
-      if (!!msg) {
-        logError('Stream has issue', msg)
-      }
-      eventBus.emit(Event.ERROR_STREAM, msg)
-      eventBus.emit(Event.ERROR, msg)
-      onDisconnect()
-    }
-
     const url = `${configurations.baseUrl}/stream?cluster=${clusterIdentifier}`
-
-    const sseHeaders = {
+    const headers = {
       'Cache-Control': 'no-cache',
       'Accept': 'text/event-stream',
       'API-Key': apiKey,
       ...standardHeaders
     }
 
-    logDebug('SSE HTTP start request', url);
+    const request = new Request(url, {
+      method: "GET",
+      headers: new Headers(headers),
+    });
 
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url)
-    for (const [header, value] of Object.entries(sseHeaders)) {
-      xhr.setRequestHeader(header, value)
-    }
-    xhr.timeout = 24 * 60 * 60 * 1000 // Force SSE to reconnect after 24hrs
-    xhr.onerror = () => {
-      onFailed('XMLHttpRequest error on SSE stream');
-    }
-    xhr.onabort = () => {
-      logDebug('SSE aborted');
-      onFailed(null);
-    }
-    xhr.ontimeout = () => {
-      onFailed('SSE timeout');
-    }
-    xhr.onload = () => {
-      if (xhr.status >= 400 && xhr.status <= 599) {
-        onFailed(`HTTP code ${xhr.status}`);
-        return;
-      }
-      onConnected();
-    }
 
-    let offset = 0;
-    let lastActivity = Date.now()
-
-    xhr.onprogress = (event) => {
-      lastActivity = Date.now()
-      const data = xhr.responseText.slice(offset);
-      offset += data.length;
-      logDebug("SSE GOT: " + data)
-      processData(data);
-    }
-
-    const readTimeoutCheckerId = setInterval(() => {
-      // this task will kill and restart the SSE connection if no data or heartbeat has arrived in a while
-      if (lastActivity < Date.now() - SSE_TIMEOUT_MS) {
-        logDebug("SSE read timeout")
-        xhr.abort()
-      }
-    }, SSE_TIMEOUT_MS)
-
-    xhr.send()
+    readStream(request, processData);
   }
+
+  async function readStream(request, dataCallback) {
+
+    try {
+
+      const resp = await fetch(request); // TODO figure out why this hangs on FF (116.0.2) when an error HTTP code is returned
+      logDebug("Got RESP " + resp.status)
+      if (resp.ok) {
+        const reader = resp.body.getReader()
+        const decoder = new TextDecoder();
+        while (true) {
+          const {done, value} = await reader.read();
+          const data = decoder.decode(value, {stream: true}); // TODO check value == undefined
+          if (done) {
+            logDebug("last chunk " + data)
+            dataCallback(data);
+            return
+          }
+          logDebug("next chunk " + data)
+          dataCallback(data);
+        }
+      } else {
+        logError("Failed to connect to SSE stream HttpCode=" + resp.status + " HttpStatus=" + resp.statusText);
+      }
+    } catch (err) {
+      logError("SSE stream failed: " + err);
+    }
+  }
+
 
   const on: EventOnBinding = (event, callback) =>
     eventBus.on((event as unknown) as EventType, (callback as unknown) as WildcardHandler)
