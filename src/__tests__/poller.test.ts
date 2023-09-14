b0b6d3a45f3dc19315ca27362a97a7395d3c54a5
Erdi Rowlands <erdi.rowlands@harness.io>
2023-09-14 14:33:58 +0100

    FFM-9030 Update test cases for sleep

--- src/__tests__/poller.test.ts
+++ src/__tests__/poller.test.ts
@@ -1,6 +1,6 @@
 import Poller from '../poller'
 import type { Options } from '../types'
-import {getRandom} from "../utils";
+import { getRandom } from '../utils'
 
 jest.useFakeTimers()
 
@@ -9,10 +9,15 @@ jest.mock('../utils.ts', () => ({
 }))
 
 interface PollerArgs {
-  fetchFlags: jest.MockedFunction<() => Promise<any>>;
+  fetchFlags: jest.MockedFunction<() => Promise<any>>
   configurations: Partial<Options>
   pollInterval: number
   maxAttempts: number
+}
+
+interface TestArgs {
+  delayFunction: jest.MockedFunction<() => number>
+  delayMs: number
 }
 
 const getPoller = (overrides: Partial<PollerArgs> = {}): { poller: Poller; pollerArgs: PollerArgs } => {
@@ -27,6 +32,16 @@ const getPoller = (overrides: Partial<PollerArgs> = {}): { poller: Poller; polle
   return {
     poller: new Poller(args.fetchFlags, args.configurations, args.pollInterval),
     pollerArgs: args
+  }
+}
+
+const getTestArgs = (overrides: Partial<TestArgs> = {}): TestArgs => {
+  const { delayMs = 5000 } = overrides // Extract delayMs or set a default of 5000
+
+  return {
+    delayMs,
+    delayFunction: (getRandom as jest.Mock).mockReturnValue(delayMs),
+    ...overrides
   }
 }
 
@@ -48,10 +63,22 @@ describe('Poller', () => {
     const fetchFlags = pollerArgs.fetchFlags
     fetchFlags.mockResolvedValue(new Error('Fetch Error'))
 
+    const testArgs = getTestArgs()
+
+
     const logSpy = jest.spyOn(poller as any, 'logDebug')
 
     poller.start()
-    jest.advanceTimersByTime(pollerArgs.maxAttempts * pollerArgs.pollInterval)
+
+    jest.advanceTimersByTime(pollerArgs.pollInterval)
+
+    // Allow first attempt to resolve
+    await Promise.resolve()
+
+    // Advance past the first delay
+    jest.advanceTimersByTime(testArgs.delayMs)
+
+    // Allow successful attempt to resolve
     await Promise.resolve()
 
     expect(fetchFlags).toHaveBeenCalledTimes(2)
@@ -59,7 +86,6 @@ describe('Poller', () => {
   })
 
   it('should not retry after max attempts are exceeded', async () => {
-
     const { poller, pollerArgs } = getPoller()
     const fetchFlags = pollerArgs.fetchFlags
     const pollInterval = pollerArgs.pollInterval
@@ -68,10 +94,7 @@ describe('Poller', () => {
       return Promise.resolve(new Error('Fetch flags Error'))
     })
 
-    const attemptDelay = 5000;
-    (getRandom as jest.Mock).mockReturnValue(attemptDelay)
-
-
+    const testArgs = getTestArgs()
 
     const logSpy = jest.spyOn(poller as any, 'logDebug')
 
@@ -80,11 +103,10 @@ describe('Poller', () => {
     jest.advanceTimersByTime(pollInterval)
 
     for (let i = 0; i < maxAttempts; i++) {
-      jest.advanceTimersByTime(attemptDelay)
+      jest.advanceTimersByTime(testArgs.delayMs)
       // We need to wait for the fetchFlags promise and the timeout promise to resolve
       await Promise.resolve()
       await Promise.resolve()
-
     }
 
     expect(fetchFlags).toHaveBeenCalledTimes(5)
