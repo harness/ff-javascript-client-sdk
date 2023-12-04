c846738411860fa1b685fd9b3faae71794eb5ee5
Kevin Nagurski <Kevin.nagurski@harness.io>
2023-12-04 11:30:25 +0000

    feat: [FFM-10087]: Add Harness-Target header (#110)

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ import { Streamer } from './stream'
 import { getVariation } from './variation'
 import Poller from './poller'
 
-const SDK_VERSION = '1.16.0'
+const SDK_VERSION = '1.21.0'
 const SDK_INFO = `Javascript ${SDK_VERSION} Client`
 const METRICS_VALID_COUNT_INTERVAL = 500
 const fetch = globalThis.fetch
@@ -307,6 +307,13 @@ const initialize = (apiKey: string, target: Target, options?: Options): Result =
         'Harness-AccountID': decoded.accountID,
         'Harness-EnvironmentID': decoded.environmentIdentifier,
         'Harness-SDK-Info': SDK_INFO
+      }
+
+      const targetHeader = btoa(JSON.stringify(target))
+
+      // ensure encoded target is less than 1/4 of 1 MB
+      if (targetHeader.length < 262144) {
+        standardHeaders['Harness-Target'] = targetHeader
       }
 
       logDebug('Authenticated', decoded)
